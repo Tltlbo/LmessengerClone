27d27dd7aafcacffdf92909b27ce4511242b1251
TLtlbo <js1436kt@gmail.com>
2024-02-03 23:59:09 +0900

    git ignore 적용

--- LMessenger/AppDelegate.swift
+++ LMessenger/AppDelegate.swift
@@ -5,4 +5,14 @@
 //  Created by 박진성 on 2/3/24.
 //
 
-import Foundation
+import UIKit
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
