1e21694c926f7a18ce69889f1446014b7ab26375
TLtlbo <js1436kt@gmail.com>
2024-02-04 17:42:47 +0900

    로그인 UI 구현 ButtonStyle 프로토콜을 이용해 자주 사용되는 버튼의 모습을 지정하여 간편하게 구현 뒤로가기 버튼을 커스텀하게 구현

---
+++ LMessenger/View/Login/LoginButtonStyle.swift
@@ -0,0 +1,8 @@
+//
+//  LoginButtonStyle.swift
+//  LMessenger
+//
+//  Created by 박진성 on 2/4/24.
+//
+
+import Foundation
---
+++ LMessenger/View/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  LMessenger
+//
+//  Created by 박진성 on 2/4/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
